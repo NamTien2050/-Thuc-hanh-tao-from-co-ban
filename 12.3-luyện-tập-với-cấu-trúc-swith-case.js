d6ff819f04d7a6e2236112ede23815e751534b88
NamTien2050 <77038541+NamTien2050@users.noreply.github.com>
2021-01-13 14:43:09 +0700

    Add files via upload

---
+++ 12.3-luyện-tập-với-cấu-trúc-swith-case.js
@@ -0,0 +1,11 @@
+let value=prompt('type a number','');
+switch (value){
+    case '0':
+    document.getElementById('nam').innerHTML=0;
+    break;
+    case '-5':
+        document.getElementById('nam').innerHTML=-1;
+        break;
+    case '5':
+        document.getElementById('nam').innerHTML=1;
+        break;}
